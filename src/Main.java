fa2d9cb002085353a2486abc259a6ea2cce32d30
Lyubov Malikova <lu.malikova58@gmail.com>
2022-11-13 23:40:24 +0300

    2 years

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         int sk = 1_000_000; // сумма кредита
-        int time = 1; // кол-во лет
+        int time = 2; // кол-во лет
         double percent = 9.9; // процентная ставка в год
         double p = service.calculate(sk, time, percent);
         System.out.println("Ежемесячный аннуитетный платеж - " + p + " рублей");
